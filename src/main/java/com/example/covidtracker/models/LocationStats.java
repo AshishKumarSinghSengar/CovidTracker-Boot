62bc274da01d8be00c78af32c2fb01aa23554447
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-01-20 10:31:31 +0530

    Updated src files

---
+++ src/main/java/com/example/covidtracker/models/LocationStats.java
@@ -0,0 +1,35 @@
+package com.example.covidtracker.models;
+
+public class LocationStats {
+
+    private String state;
+    private String country;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return LatestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        LatestTotalCases = latestTotalCases;
+    }
+
+    private int LatestTotalCases;
+
+
+}
